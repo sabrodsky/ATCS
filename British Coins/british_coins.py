8ef9c11853ddb2b25cbebc2b89470abf586b36fd
sabrodsky <sabrodsky@packer.edu>
2020-11-10 20:22:56 -0500

    British Coins Problem

---
+++ British Coins/british_coins.py
@@ -0,0 +1,13 @@
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+goal = 200
+solutions = [1] + ([0] * goal)
+for coin in coins:
+	for j in range(coin, goal + 1):
+		solutions[j] += solutions[j - coin]
+
+print(solutions[goal])
+
+# https://blog.dreamshire.com/project-euler-31-solution/
+# I think this code uses partitions from number theory
+# I didn't go in-depth but it sounds interesting
+# although this may count doubles, I'm not sure.
