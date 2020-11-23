722f49108ef1ffa0143d58f48f6a74d402230cdc
sabrodsky <sabrodsky@packer.edu>
2020-11-23 01:54:16 -0500

    made the design fit to every window size (hopefully)

--- Pygame/interesting_design.py
+++ Pygame/interesting_design.py
@@ -1,12 +1,12 @@
-#you will need to install pygame of course
+# you will need to install pygame of course
 import pygame, random
 
 #launch the pygame module
 pygame.init()
 
-# Set up the drawing window
-size = (500,500)
-screen = pygame.display.set_mode(size)
+# Set up the drawing window (every aspect changes dynamically so you can easily change window size)
+window_x, window_y = 600,600
+screen = pygame.display.set_mode((window_x,window_y))
 
 # Run until the user asks to quit
 running = True
@@ -17,11 +17,8 @@ color_count = 0
 colors = {'white': (255,255,255), 'black': (0,0,0), 'red': (255,0,0), 'green': (0,255,0), 'pine': (50, 125, 50), 'blue': (0,0,255), 'yellow': (255,255,0), 'magenta': (255,0,255), 'cyan': (0,255,255), 'brown': (120,60,30), 'orange': (255,150,0)}
 ornament_list = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'magenta']
 
-# Tree setup
-tree_triangles = [[(250,50),(150,150),(350,150)],[(250,100),(125,225),(375,225)],[(250,150),(100,300),(400,300)],[(250,200),(75,375),(425,375)],[(250,250),(50,450),(450,450)]]
-ornament_centers_bottom = [[(80,440),0],[(105, 430),1],[(130,420),2],[(155,410),3],[(180,400),4],[(205,390),5],[(230,380),6],[(255,370),7],[(280,360),8],[(305,350),9],[(330,340),10],[(355,330),11]]
-ornament_centers_mid = [[(135,285),7],[(160, 275),8],[(185,265),9],[(210,255),10],[(235,245),11],[(260,235),12],[(285,225),13],[(310,215),14],[(335,205),15]]
-ornament_centers_top = [[(180,140),1],[(205, 130),2],[(230,120),3],[(255,110),4],[(280,100),5]]
+# Ratio (it's 500 by 500 because that was the window size that I had originally used to calculate everything)
+x_ratio, y_ratio = (window_x/500), (window_y/500)
 
 # snow setup
 class Snowflake:
@@ -29,7 +26,7 @@ class Snowflake:
         self.pos_x = x
         self.pos_y = y
         self.change_x = random.randint(1,4)
-        self.change_y = random.randint(1,3)
+        self.change_y = random.randint(1,5)
     def move_snowflake(self):
         self.change_x *= -1
         self.pos_x += self.change_x
@@ -45,30 +42,34 @@ while running:
             running = False
 
     # Fill the background with white
-    # screen.fill((225,245,255))
     screen.fill(colors['black'])
 
-    if snow_count%10 == 0:
-        snowflakes.append(Snowflake(((snow_count * 19)%500),0))
+    #add snowflakes to list
+    if snow_count%7 == 0:
+        snowflakes.append(Snowflake(random.randint(5, (window_x-5)),0))
+    #move and draw snowflakes
     for snowflake in snowflakes:
         snowflake.move_snowflake()
-        snowflake.draw_snowflake(snowflake.pos_x, snowflake.pos_y)
+        if snowflake.pos_y >= (window_y + 5):
+            snowflakes.remove(snowflake)
+        else:
+            snowflake.draw_snowflake(snowflake.pos_x, snowflake.pos_y)
 
     # Drawing Tree
-    pygame.draw.rect(screen, colors['brown'], [230, 450, 40, 50])
-    for triangle in tree_triangles:
-        pygame.draw.polygon(screen, colors['pine'], triangle)
+    pygame.draw.rect(screen, colors['brown'], [230*x_ratio, 450*y_ratio, 40*x_ratio, 50*y_ratio])
+    for x in range(5):
+        pygame.draw.polygon(screen, colors['pine'], [(250*x_ratio, (50*y_ratio)+(x*50*y_ratio)),((150*x_ratio)-(x*25*x_ratio),(150*y_ratio)+(x*75*y_ratio)),((350*x_ratio)+(x*25*x_ratio),(150*y_ratio)+(x*75*y_ratio))])
 
     # Draw Star
-    pygame.draw.polygon(screen, colors['yellow'], [(250,15),(260,35),(285,35),(265,50),(275,75),(250,60),(225,75),(235,50),(215,35),(240,35)])
+    pygame.draw.polygon(screen, colors['yellow'], [(250*x_ratio,15*y_ratio),(260*x_ratio,35*y_ratio),(285*x_ratio,35*y_ratio),(265*x_ratio,50*y_ratio),(275*x_ratio,75*y_ratio),(250*x_ratio,60*y_ratio),(225*x_ratio,75*y_ratio),(235*x_ratio,50*y_ratio),(215*x_ratio,35*y_ratio),(240*x_ratio,35*y_ratio)])
 
     # Draw Ornaments
-    for center in ornament_centers_bottom:
-        pygame.draw.circle(screen, colors[ornament_list[(color_count+center[1])%len(ornament_list)]], center[0], 10)
-    for center in ornament_centers_mid:
-        pygame.draw.circle(screen, colors[ornament_list[(color_count+center[1])%len(ornament_list)]], center[0], 10)
-    for center in ornament_centers_top:
-        pygame.draw.circle(screen, colors[ornament_list[(color_count+center[1])%len(ornament_list)]], center[0], 10)
+    for x in range(5): #top row
+        pygame.draw.circle(screen, colors[ornament_list[(color_count+x+12)%len(ornament_list)]], ((180*x_ratio)+(x*(25*x_ratio)),(138*y_ratio)-(x*(10*y_ratio))), 10)
+    for x in range(9): #mid row
+        pygame.draw.circle(screen, colors[ornament_list[(color_count+x+5)%len(ornament_list)]], ((135*x_ratio)+(x*(25*x_ratio)),(285*y_ratio)-(x*(10*y_ratio))), 10)
+    for x in range(12): #bottom row
+        pygame.draw.circle(screen, colors[ornament_list[(color_count+x+9)%len(ornament_list)]], ((80*x_ratio)+(x*(25*x_ratio)),(438*y_ratio)-(x*(10*y_ratio))), 10)
 
     # Pause
     pygame.display.flip()
