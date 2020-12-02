4803a003f8ac8d26ebed913f55234c0f3bdd3c25
sabrodsky <sabrodsky@packer.edu>
2020-12-01 20:27:27 -0500

    fixed minute hand

--- Classes/clock.py
+++ Classes/clock.py
@@ -22,14 +22,14 @@ class Hour:
     def __init__(self, x, y, hours):
         self.x = x
         self.y = y
-        self.pos = hours - 15
+        self.pos = hours
     def move(self, seconds):
         if int(seconds - 60-current_time.second)%60 == 0:
             self.pos += 1/60
     def show(self, seconds):
         self.move(seconds)
         pygame.draw.line(screen, (0,0,255), (self.x, self.y), (self.x+125*math.cos(2*math.pi*(self.pos/360)), self.y+125*math.sin(2*math.pi*(self.pos/360))), 5)
-hour_hand = Hour(window_x/2, window_y/2, current_time.hour)
+hour_hand = Hour(window_x/2, window_y/2, (current_time.hour + (-5))%12)
 
 #minute hand
 class Minute:
@@ -37,7 +37,7 @@ class Minute:
         self.x = x
         self.y = y
         self.pos = minutes - 15
-        self.offset = 60 - current_time.second #makes sure the minute hand only updates when second hand is at 12
+        self.offset = 59 - current_time.second #makes sure the minute hand only updates when second hand is at 12
     def move(self, seconds):
         if int(seconds - self.offset)%60 == 0: #only happens if second hand is at 12
             self.pos += 1
@@ -72,8 +72,11 @@ while running:
     screen.fill((255, 255, 255))
 
     # dots for the numbers on the clock (couldn't figure out text)
-    for x in range(12):
-        pygame.draw.line(screen, (0,0,0), (250 + 175*math.cos(2*math.pi*(x/12)), 250 + 175*math.sin(2*math.pi*(x/12))), (250 + 180*math.cos(2*math.pi*(x/12)), 250 + 180*math.sin(2*math.pi*(x/12))), 3)
+    for x in range(60):
+        if x%5 == 0:
+            pygame.draw.line(screen, (0,0,0), (250 + 160*math.cos(2*math.pi*(x/60)), 250 + 160*math.sin(2*math.pi*(x/60))), (250 + 180*math.cos(2*math.pi*(x/60)), 250 + 180*math.sin(2*math.pi*(x/60))), 6)
+        else:
+            pygame.draw.circle(screen, (0,0,0), (250 + 180*math.cos(2*math.pi*(x/60)), 250 + 180*math.sin(2*math.pi*(x/60))), 3)
 
     # Draw outline of clock
     pygame.draw.circle(screen, (0,0,0), (window_x/2, window_y/2), 200, 5)
